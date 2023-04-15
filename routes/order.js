3f09ce58b76f5a89e210d93fe3fcd5b392ca31d1
unknown <hrishikeshmandule@gmail.com>
2023-04-15 10:35:21 +0530

    Initial Commit

---
+++ routes/order.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { authorizeAdmin, isAuthenticated } from "../middleware/auth.js";
+import { getAdminOrders, getMyOrders, getOrderDetails, paymentVerification, placeOrder, placeOrderOnline, processOrder } from "../controllers/order.js";
+
+const router = express.Router();
+
+
+router.post("/createorder", isAuthenticated, placeOrder);
+
+router.post("/createorderonline",placeOrderOnline)
+
+router.post("/paymentVerification", paymentVerification)
+
+router.get("/myorders",isAuthenticated,getMyOrders);
+
+router.get("/order/:id", isAuthenticated,getOrderDetails)
+
+// Add Admin Middleware
+router.get("/admin/orders", isAuthenticated, authorizeAdmin, getAdminOrders)
+
+router.get("/admin/order/:id", isAuthenticated, authorizeAdmin, processOrder)
+
+
+export default router;
